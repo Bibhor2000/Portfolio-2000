21ecfc87d824619692c452e43128d5996c183491
RocketPanda818 <bguru002@odu.edu>
2023-01-17 13:47:18 -0500

    added transparent styling to all buttons

--- pages/projects.js
+++ pages/projects.js
@@ -13,10 +13,10 @@ export default function Home() {
                     view them once uploaded.
                 </p>
                 <Link href='https://github.com/Bibhor2000/3DView'>
-                    <button>Visit Repo</button>
+                    <button className={styles.cardbuttons}>Visit Repo</button>
                 </Link>
                 <Link href='https://3d-view.vercel.app/'>
-                    <button>Visit Live Site</button>
+                    <button className={styles.cardbuttons}>Visit Live Site</button>
                 </Link>
             </div>
             <div className={styles.card}>
@@ -25,20 +25,20 @@ export default function Home() {
                     with many choices for customization.
                 </p>
                 <Link href='https://github.com/Bibhor2000/Project-2'>
-                    <button>Visit Repo</button>
+                    <button className={styles.cardbuttons}>Visit Repo</button>
                 </Link>
                 <Link href='http://character-creator-indol.vercel.app/'>
-                    <button>Visit Live Site</button>
+                    <button className={styles.cardbuttons}>Visit Live Site</button>
                 </Link>
             </div>
             <div className={styles.card}>
                 <h3>Hangman Variation</h3>
                 <p>An variation of the Hangman Game</p>
                 <Link href='https://github.com/Bibhor2000/Project-1'>
-                    <button>Visit Repo</button>
+                    <button className={styles.cardbuttons}>Visit Repo</button>
                 </Link>
                 <Link href='https://bibhor2000.github.io/Project-1/'>
-                    <button>Visit Live Site</button>
+                    <button className={styles.cardbuttons}>Visit Live Site</button>
                 </Link>
             </div>
         </div>
