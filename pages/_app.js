a1dbc119b4e46f7718657a417fb8f0a47e0c100a
RocketPanda818 <bguru002@odu.edu>
2023-01-23 15:28:36 -0500

    added image and readjusted styling on the about page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
 import Navbar from '../comps/nav';
+import axios from 'axios';
 
 function MyApp({ Component, pageProps }) {
+
+  const axios = require('axios');
+  const params = {
+    access_key: '893152ff4c97608e3c1affe8432ac063',
+    query: 'Virginia'
+  }
+
+  axios.get('https://api.weatherstack.com/current', {params})
+    .then(response => {
+      const apiResponse = response.data;
+      console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
+    }).catch(error => {
+      console.log(error);
+    });
+
+
   return (
     <div>
       <Navbar/> 
--- pages/about.js
+++ pages/about.js
@@ -5,12 +5,17 @@ import Image from 'next/image';
 export default function Home() {
 
     return (
-        <div>
-            <div>
-                Hi, My name is Bibhor Gurung and I am on my own journey to learn <br></br>
-                about web & game development.<br></br>
-                What sparked my interest in web/game development <br></br>
-                is my desire to make my own game in my own website. <br></br>
+        <div className={styles.appAboutContainer}>
+            <div className={styles.appAbout}>
+                Hi, My name is Bibhor Gurung and I am on my own<br></br>
+                <br></br>journey to learn about web & game development.<br></br>
+                <br></br>What sparked my interest in web/game development<br></br>
+                <br></br>is my desire to make my own game in my own website.<br></br>
+                <br></br>So far most of my experience involved Javascript<br></br>
+                <br></br>frameworks like React, Node, & ExpressJs.  
+            </div>
+            <div className={styles.appAboutImage}>
+                <img src='/portrait.jpg'/>
             </div>
         </div>
     )
